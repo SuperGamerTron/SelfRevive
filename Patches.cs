a9ab47a66b9aa58d7d5ebdfe191fbf2bf1109a46
SuperGamerTron <45374546+SuperGamerTron@users.noreply.github.com>
2021-07-02 19:36:50 -0600

    Add files via upload

---
+++ Patches.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace SelfRevive.Patches
+{
+    public class MuckPatch
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(ChatBox), nameof(ChatBox.Instance.ChatCommand))]
+        public static void OnChatCommand(ref string message)
+        {
+            if (message == "/revive")
+            {
+                PlayerManager playerManager = GameManager.players[LocalClient.instance.myId];
+                if (playerManager.dead && GameManager.state == GameManager.GameState.GameOver && GameManager.players.Count == 1)
+                {
+                    ClientSend.RevivePlayer(playerManager.id);
+                    GameManager.instance.gameoverUi.SetActive(false);
+                    Cursor.visible = false;
+                    Cursor.lockState = CursorLockMode.Locked;
+                    GameManager.state = GameManager.GameState.Playing;
+                }
+            }
+        }
+    }
+}
