84effd3edebd41f600745d4042d287746710240f
SuperGamerTron <45374546+SuperGamerTron@users.noreply.github.com>
2021-07-02 21:43:44 -0600

    Add files via upload

---
+++ Patches.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SelfRevive.Patches
+{
+    public class MuckPatch
+    {
+        [HarmonyPostfix, HarmonyPatch(typeof(GameManager), nameof(GameManager.ShowEndScreen))]
+        public static void OnShowEndScreen()
+        {
+            if (GameManager.players.Count == 1)
+            {
+                if (SelfRevive.gameoverUI == null)
+                {
+                    SelfRevive.gameoverUI = GameObject.Find("UI (1)/GameoverUi").gameObject;
+                    GameObject menuButton = SelfRevive.gameoverUI.transform.Find("MenuButton").gameObject;
+                    GameObject reviveButton = Object.Instantiate(menuButton, SelfRevive.gameoverUI.transform);
+                    menuButton.transform.position = new Vector3(menuButton.transform.position.x + 400, menuButton.transform.position.y, menuButton.transform.position.z);
+                    reviveButton.transform.position = new Vector3(reviveButton.transform.position.x - 400, reviveButton.transform.position.y, reviveButton.transform.position.z);
+                    reviveButton.GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
+                    reviveButton.GetComponent<Button>().onClick.AddListener(() => SelfRevive.Revive());
+                    reviveButton.transform.Find("RawImage").GetComponentInChildren<TMP_Text>().text = "Revive";
+                }
+            }
+        }
+    }
+}
---
+++ SelfRevive.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using HarmonyLib;
+using SelfRevive.Patches;
+using UnityEngine;
+
+namespace SelfRevive
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInProcess("Muck.exe")]
+    public class SelfRevive : BaseUnityPlugin
+    {
+        public const string
+            GUID = "SelfRevive",
+            NAME = "SelfRevive",
+            VERSION = "1.0.0";
+
+        public static GameObject gameoverUI;
+
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(MuckPatch));
+            Logger.LogMessage("Loaded SelfRevive");
+        }
+
+        public static void Revive()
+        {
+            PlayerManager playerManager = GameManager.players[LocalClient.instance.myId];
+            if (playerManager.dead && GameManager.state == GameManager.GameState.GameOver)
+            {
+                ClientSend.RevivePlayer(playerManager.id);
+                GameManager.instance.gameoverUi.SetActive(false);
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                GameManager.state = GameManager.GameState.Playing;
+            }
+        }
+    }
+}
