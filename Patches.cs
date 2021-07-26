09bc6895c1b10a342fb1e7bb8e1639ccda5ec832
SuperGamerTron <oomonkeygaming@gmail.com>
2021-07-26 15:25:41 -0600

    Update Patches.cs

--- Patches.cs
+++ Patches.cs
@@ -14,14 +14,15 @@ namespace SelfRevive.Patches
             {
                 if (SelfRevive.gameoverUI == null)
                 {
-                    SelfRevive.gameoverUI = GameObject.Find("UI (1)/GameoverUi").gameObject;
+                    SelfRevive.gameoverUI = GameObject.Find("UI (1)/GameoverUi/Overlay/Header");
                     GameObject menuButton = SelfRevive.gameoverUI.transform.Find("MenuButton").gameObject;
                     GameObject reviveButton = Object.Instantiate(menuButton, SelfRevive.gameoverUI.transform);
-                    menuButton.transform.position = new Vector3(menuButton.transform.position.x + 400, menuButton.transform.position.y, menuButton.transform.position.z);
-                    reviveButton.transform.position = new Vector3(reviveButton.transform.position.x - 400, reviveButton.transform.position.y, reviveButton.transform.position.z);
+                    reviveButton.transform.position = new Vector3(reviveButton.transform.position.x + 160, reviveButton.transform.position.y, reviveButton.transform.position.z);
                     reviveButton.GetComponent<Button>().onClick = new Button.ButtonClickedEvent();
                     reviveButton.GetComponent<Button>().onClick.AddListener(() => SelfRevive.Revive());
-                    reviveButton.transform.Find("RawImage").GetComponentInChildren<TMP_Text>().text = "Revive";
+                    TMP_Text text = reviveButton.transform.Find("RawImage").GetComponentInChildren<TMP_Text>();
+                    text.autoSizeTextContainer = true;
+                    text.text = "Revive";
                 }
             }
         }
